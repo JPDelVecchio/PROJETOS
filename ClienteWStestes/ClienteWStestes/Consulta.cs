9be71bc870d1c623cda437aefbebdcd7e74d2ed7
jpldelvecchio <jpldelvecchio@gmail.com>
2020-05-18 12:07:09 -0300

    Signed-off-by: jpldelvecchio <jpldelvecchio@gmail.com>

---
+++ ClienteWStestes/ClienteWStestes/Consulta.cs
@@ -0,0 +1,36 @@
+﻿using ClienteWStestes.WSConsulta;
+using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClienteWStestes
+{
+    class Consulta
+    {
+        String Produto { get; set; }
+        public string ConsultarUsuario() { 
+        var ret = new  WSConsulta.wsConsultaSQLSoapClient();
+  
+            if (ret.AutenticaAcessoAuth("Mestre", "totvs") == "1"){
+                return "Existe";
+                    }
+            else{
+                return "Falso";
+                }
+            }
+        public void Produtos()
+        {
+            var ret = new wsConsultaSQLSoapClient();
+            var retorno  = ret.RealizarConsultaSQLAuth("TESTEJOAO",1,"T","Mestre","totvs", "1");
+ 
+                Console.WriteLine(retorno);
+           
+        }
+
+        }
+    }
+
---
+++ ClienteWStestes/ClienteWStestes/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClienteWStestes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cons = new Consulta();
+            
+            String retorno = cons.ConsultarUsuario();
+
+            Console.WriteLine(retorno);
+
+            Console.WriteLine("outra consulta de produtos \\n");
+
+            cons.Produtos();
+        }
+    }
+}
