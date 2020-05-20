8e7e6db0bbdf786c54b7f21ee8232bd8bacdabb8
jpldelvecchio <jpldelvecchio@gmail.com>
2020-05-20 18:42:29 -0300

    Signed-off-by: jpldelvecchio <jpldelvecchio@gmail.com>

---
+++ CRUD_basico/CRUD_basico/Classes/Item.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUD_basico.Modelos
+{
+    class Item
+    {
+        public Produto ItemNf { get; set; }
+        public int Quantidade { get; set; }
+        public double Valor { get; set; }
+
+    }
+}
